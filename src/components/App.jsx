4bcc82e2dadc4df4be03826fff2a02bdebdbd031
Victor Litvynchuk <vendettich13@icloud.com>
2022-12-02 14:10:31 +0200

    add skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,7 @@
-import { Component } from "react";
-import { Section } from "./Section/Section";
-import { FeedbackOptions } from "./Feedback/FeedbackOptions";
-import { Statistics } from "./Statistics/Statistics";
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
-  
-  onLeaveFeedback = (e) => {
-    const currentBtn = e.currentTarget.name;
-    this.setState((prevState) => {
-      return { [currentBtn]: prevState[currentBtn] + 1 }
-    })
-  }
-  total = () => {
-    const { good, bad, neutral } = this.state
-    return good + bad + neutral
-  }
-  positivePercentage = () => {
-    const {good} = this.state
-    return good / this.total() * 100;
-  }
-
-  render() {
-    return <section>
-    <Section title="Please leave feedback">
-        <FeedbackOptions options={['good', 'bad', 'neutral']} onLeaveFeedback={this.onLeaveFeedback}/>
-    </Section>
-    <Section title="Statistics">
-         <Statistics good={this.state.good} neutral={this.state.neutral } bad={this.state.bad } total={this.total} positivePercentage={this.positivePercentage}/>
-      </Section>
-      </section>
-    }
+export const App = () => {
+  return (
+    <div>
+      React homework template
+    </div>
+  );
 };
- 
